22f7a2754f37d71b1a1a8021a0c8ea20dea8adea
Omar Faruq <omarfaruq9723@gmail.com>
2026-02-01 15:28:37 +0600

    init

---
+++ LoanMngtSys/src/main/java/com/org/omar/loanMngtSys/LoanMngtSysApplication.java
@@ -0,0 +1,13 @@
+package com.org.omar.loanMngtSys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoanMngtSysApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(LoanMngtSysApplication.class, args);
+    }
+}
---
+++ LoanMngtSys/src/main/java/com/org/omar/loanMngtSys/entities/paymentEntity.java
@@ -0,0 +1,4 @@
+package com.org.omar.loanMngtSys.entities;
+
+public class paymentEntity {
+}
